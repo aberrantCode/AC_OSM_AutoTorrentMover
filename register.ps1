8f736997a6b54579eb0b53db76f63736a8c6bd7c
Erik <aberrantcode@gmail.com>
2025-03-06 08:54:29 -0500

    Update script paths and enhance error handling in scheduled task registration

--- register.ps1
+++ register.ps1
@@ -1,8 +1,8 @@
 $taskName = "Move Torrent Files"
-$scriptPath = "C:\development\AC_OSM_AutoTorrentMover\start-monitor.ps1"  # Update this path to the actual location of your start-monitor.ps1 script
+$scriptPath = "c:\osm\torrents\src\Move-Torrents.ps1"  # Update this path to the actual location of your start-monitor.ps1 script
 
 # Import the common.ps1 script
-. C:\development\AC_OSM_AutoTorrentMover\common.ps1
+. "$($PSScriptRoot)\common.ps1"
 
 Write-ToLog "Starting the register script..."
 Write-ToLog "Task name: $taskName"
@@ -69,13 +69,13 @@ function Register-ScheduledTaskTriggeredOnSchedule {
     Write-ToLog "Creating a new scheduled task with $IntervalMinutes minute interval trigger..."
 
     # Define the action to run the PowerShell script
-    $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-File `"$ScriptPath`""
+    $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-File `"$ScriptPath`"" -ErrorAction Stop
 
     # Define the event trigger for recurring schedule
-    $trigger = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Minutes $IntervalMinutes) -RepetitionDuration ([TimeSpan]::MaxValue)
+    $trigger = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Minutes $IntervalMinutes) -ErrorAction Stop
 
     # Register the scheduled task
-    Register-ScheduledTask -Action $action -Trigger $trigger -TaskName $TaskName -Description $Description
+    Register-ScheduledTask -Action $action -Trigger $trigger -TaskName $TaskName -Description $Description -ErrorAction Stop
 
     Write-ToLog "Scheduled task '$TaskName' has been created to run every $IntervalMinutes minutes."
     return $true
@@ -84,4 +84,4 @@ function Register-ScheduledTaskTriggeredOnSchedule {
 # Use the Register-ScheduledTaskTriggerAfterLogon function to register the task
 # You can uncomment one of these lines to register the task using your preferred method
 # Register-ScheduledTaskTriggerAfterLogon -TaskName $taskName -ScriptPath $scriptPath
-Register-ScheduledTaskTriggeredOnSchedule -TaskName $taskName -ScriptPath $scriptPath -IntervalMinutes 5
+Register-ScheduledTaskTriggeredOnSchedule -TaskName $taskName -ScriptPath $scriptPath -IntervalMinutes 5 | Out-Null
